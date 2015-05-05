d795e5ccf726557cacccb73c10471ddc2efee036
Michael Li <li.michael.c@gmail.com>
2015-05-05 13:46:28 -0400

    Added mouse tracking

--- js/kino.js
+++ js/kino.js
@@ -1,66 +1,80 @@
-    var scene, camera, renderer;
+var scene, camera, renderer;
 
-    init();
-    animate();
+init();
+animate();
 
-    // Sets up the scene.
-    function init() {
+var mouseX = 0, mouseY = 0;
 
-      // Create the scene and set the scene size.
-      scene = new THREE.Scene();
-      var WIDTH = window.innerWidth,
-      HEIGHT = window.innerHeight;
-
-      // Create a renderer and add it to the DOM.
-      renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
-      renderer.setSize(WIDTH, HEIGHT);
-      renderer.setClearColor( 0xffffff, 1);
-      document.body.appendChild(renderer.domElement);
-
-      // Create a camera, zoom it out from the model a bit, and add it to the scene.
-      camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 20000);
-      camera.position.set(0, 0, -20);
-      scene.add(camera);
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
 
-      // Create an event listener that resizes the renderer with the browser window.
-      window.addEventListener('resize', function() {
+// Sets up the scene.
+function init() {
+
+    // Create the scene and set the scene size.
+    scene = new THREE.Scene();
+    var WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight;
+
+    // Create a renderer and add it to the DOM.
+    renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.setClearColor( 0xffffff, 1);
+    document.body.appendChild(renderer.domElement);
+
+    // Create a camera, zoom it out from the model a bit, and add it to the scene.
+    camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 20000);
+    camera.position.set(0, 0, -20);
+    scene.add(camera);
+
+
+    // Create an event listener that resizes the renderer with the browser window.
+    window.addEventListener('resize', function() {
         var WIDTH = window.innerWidth,
         HEIGHT = window.innerHeight;
         renderer.setSize(WIDTH, HEIGHT);
         camera.aspect = WIDTH / HEIGHT;
         camera.updateProjectionMatrix();
-      });
+    });
 
-      var material = new THREE.LineBasicMaterial({
+    var material = new THREE.LineBasicMaterial({
         color: 0x0000ff
-      });
-      var geometry = new THREE.Geometry();
-      geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-      geometry.vertices.push(new THREE.Vector3(10, 0, 0));
-      geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-      var line = new THREE.Line(geometry, material);
+    });
 
-      scene.add(line);
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+    geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+    geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    var line = new THREE.Line(geometry, material);
 
-      // Create a light, set its position, and add it to the scene.
-      var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-      scene.add( light );
+    scene.add(line);
 
-      // Add OrbitControls so that we can pan around with the mouse.
-      controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // Create a light, set its position, and add it to the scene.
+    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
 
-    }
+    // Add OrbitControls so that we can pan around with the mouse.
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+}
 
 
-    // Renders the scene and updates the render as needed.
-    function animate() {
+// Renders the scene and updates the render as needed.
+function animate() {
+    // Read more about requestAnimationFrame at http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+    requestAnimationFrame(animate);
 
-      // Read more about requestAnimationFrame at http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
-      requestAnimationFrame(animate);
-      
-      // Render the scene.
-      renderer.render(scene, camera);
-      controls.update();
+    // Render the scene.
+    renderer.render(scene, camera);
+    controls.update();
 
-    }
+}
+
+function onDocumentMouseMove( event ) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+    console.log(mouseX, mouseY);
+}
